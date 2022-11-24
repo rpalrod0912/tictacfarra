20b0c2e75f135793dee6307dff5671da4f0fe9c0
rpalrod0912 <rpalrod0912@g.educaand.es>
2022-11-24 08:57:23 +0100

    "Añadido Metodo handleClick"

--- src/index.js
+++ src/index.js
@@ -25,6 +25,12 @@ class Square extends React.Component {
             };
         }
     
+        handleClick(i){
+            const squares = this.state.squares.slice();
+            squares[i] = 'X';
+            this.setState({squares:squares});
+        }
+    
     renderSquare(i) {
       return <Square value={this.state.squares[i]} onClick={()=> this.handleClick(i)}/>;
     }
